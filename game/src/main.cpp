6961f5dc8c88d488558a15ed91b73d8453065cf9
David Lopez <davidl.a@hotmail.es>
2023-01-31 18:32:35 +0100

    Add project files.

---
+++ game/src/main.cpp
@@ -0,0 +1,14 @@
+#include "raylib.h"
+#include "screens.h"    // NOTE: Declares global (extern) variables and screens functions
+#include <iostream>
+
+#if defined(PLATFORM_WEB)
+    #include <emscripten/emscripten.h>
+#endif
+
+int main(void)
+{
+
+    system("pause");
+    return 0;
+};
