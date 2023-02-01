364727742e9d68e41093c745c66d32637184b96b
David Lopez <davidl.a@hotmail.es>
2023-02-01 18:36:13 +0100

    Cambios al flipbook

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -2,12 +2,41 @@
 #include "screens.h"    // NOTE: Declares global (extern) variables and screens functions
 #include <iostream>
 #include <main.h>
+#include <string.h>
 
 int main(void)
 {
-    FlipBook F1(2);
-    *F1.Sprites[0] = LoadTexture("Esto es un test. Cargar textura aqui."); //Se desreferencia el puntero para poder acceder al contenido.
-    *F1.Sprites[1] = LoadTexture("Esto es un test. Cargar textura aqui.");
-    system("pause");
+    int WindowWidth = 960;
+    int WindowHeigh = 540;
+
+    FlipBook FB_BackGround("resources/SpriteSheets/BackGround/testSheet.png", WindowWidth/2, WindowHeigh/2, 0.1f, WHITE);
+    Rectangle BackGroundRec[2] = {
+    {0.0, 0.0, 1152, 648},
+    {0.0, 0.0, 1152, 648}
+    }; //Creacion de matriz que va ligada a la funcion "void PlayFlipBook" que asigna los puntos de la textura (Falta asignarlos)
+    
+    bool globalRunning = true;
+    SetTargetFPS(165);
+    InitWindow(WindowWidth, WindowHeigh, "Car Game");
+    while (globalRunning)
+    {    
+        
+        BeginDrawing();
+        DrawFPS(850,20);
+
+        FB_BackGround.PlayFlipBook(BackGroundRec);
+
+        EndDrawing();
+        ClearBackground(BLACK);
+
+        //Verificar si se debe cerrar la ventana al pulsar ESC o la X.
+        if (WindowShouldClose()) {
+            globalRunning = false;
+            CloseWindow();
+        };
+    };
+
+    FB_BackGround.PlayFlipBook(BackGroundRec);
+
     return 0;
 }; 
--- game/src/main.h
+++ game/src/main.h
@@ -3,28 +3,43 @@
 class FlipBook
 {
 private:
-	int TotalSprites; //Total de sprites que contiene el FlipBook.
+	Texture2D SpriteSheet; 
+	Vector2 Position;
+	Color SpritesColor;
+	int CurrentFrame;
+	float CurrentTime;
+	float FrameSpeed;
 public:
 	
-	Texture2D** Sprites; //El doble asterisco me permite crear un puntero en el Stack del procesador que almacene varios punteros de tipo Texture2D.
-	FlipBook(int TotalSprites_) {
-		TotalSprites = TotalSprites_;
-		Sprites = new Texture2D * [TotalSprites]; //Creo un array de punteros en la memoria RAM (heap) de tipo Texture2D.
+	FlipBook(char* TextureUrl, int PosX, int PosY, float FrameSpeed_, Color SpritesColor_) {
+		
+		SpriteSheet = LoadTexture(TextureUrl);
 
-		for (int i = 0; i < TotalSprites; i++)
-		{
-			Sprites[i] = new Texture2D; //Asigno al array de punteros una instancia de tipo Texture2D.
-		};
+		Position.x = PosX;
+		Position.y = PosY;
 
+		SpritesColor = SpritesColor_;
+
+		FrameSpeed = FrameSpeed_;
+		CurrentFrame = 0;
+		CurrentTime = 0.0f;
+			
 	};
 	~FlipBook() {
-		for (int i = 0; i < TotalSprites; i++) //Este bucle destruye el array de punteros de tipo Texture2D de la RAM (heap).
-		{
-			delete Sprites[i];
-		};
-		delete Sprites; //Aqui destruimos el puntero que contiene los demas punteros (aunque es innecesario realmente porque al salir del scope ya se elimina del stack).
+		UnloadTexture(SpriteSheet);
 	};
-
-	//TO-DO: Crear funcion(es) que muevan el sprite por frame.
+	void PlayFlipBook(Rectangle* SpritesPos) {
+			DrawTextureRec(SpriteSheet, SpritesPos[CurrentFrame], Position, SpritesColor);			
+			CurrentTime += FrameSpeed;
+			if (CurrentTime > 1) {
+				CurrentFrame++;
+				CurrentTime = 0.0f;
+		};
+			if (CurrentFrame > sizeof(*SpritesPos) / sizeof(SpritesPos[0])) {
+				CurrentFrame = 0;
+			};
+			
+	};
+	
 
 };
