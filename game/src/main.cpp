5eaecb948a02305e421261e122ac0d20e5e27c5b
David Lopez <davidl.a@hotmail.es>
2023-02-01 02:52:04 +0100

    Texture load inside FlipBook

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -5,7 +5,9 @@
 
 int main(void)
 {
-
+    FlipBook F1(2);
+    *F1.Sprites[0] = LoadTexture("Esto es un test. Cargar textura aqui."); //Se desreferencia el puntero para poder acceder al contenido.
+    *F1.Sprites[1] = LoadTexture("Esto es un test. Cargar textura aqui.");
     system("pause");
     return 0;
 }; 
--- game/src/main.h
+++ game/src/main.h
@@ -3,22 +3,28 @@
 class FlipBook
 {
 private:
-	int TotalFrames;
-	Texture2D* Sprites;
+	int TotalSprites; //Total de sprites que contiene el FlipBook.
 public:
 	
+	Texture2D** Sprites; //El doble asterisco me permite crear un puntero en el Stack del procesador que almacene varios punteros de tipo Texture2D.
+	FlipBook(int TotalSprites_) {
+		TotalSprites = TotalSprites_;
+		Sprites = new Texture2D * [TotalSprites]; //Creo un array de punteros en la memoria RAM (heap) de tipo Texture2D.
 
-	FlipBook(int TotalFrames_) {
-		TotalFrames_ = TotalFrames;
-		Sprites = new Texture2D[TotalFrames];
-
-		for (int i = 0; i < TotalFrames; i++)
-		{	
-			Sprites[i] = LoadTexture("");
-		}
+		for (int i = 0; i < TotalSprites; i++)
+		{
+			Sprites[i] = new Texture2D; //Asigno al array de punteros una instancia de tipo Texture2D.
+		};
 
 	};
 	~FlipBook() {
-		delete Sprites;
+		for (int i = 0; i < TotalSprites; i++) //Este bucle destruye el array de punteros de tipo Texture2D de la RAM (heap).
+		{
+			delete Sprites[i];
+		};
+		delete Sprites; //Aqui destruimos el puntero que contiene los demas punteros (aunque es innecesario realmente porque al salir del scope ya se elimina del stack).
 	};
+
+	//TO-DO: Crear funcion(es) que muevan el sprite por frame.
+
 };
