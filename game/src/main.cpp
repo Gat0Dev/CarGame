0c4148144324c65c9a51072102e30136ae1a7359
David Lopez <davidl.a@hotmail.es>
2023-02-10 02:34:14 +0100

    Static Enemy Vehicle working.
    - Worked on C_Enemy_Vehicle.
    - Vehicle moves, destroys and spawns.

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -9,216 +9,227 @@ bool globalRunning = true; //Variable tratada como global para acceder al cierre
 
 int main(void)
 {
-    //Inicializacion de ventana (Y OpenGL).
-    int WindowWidth = 960;
-    int WindowHeigh = 540;
-    InitWindow(WindowWidth, WindowHeigh, "Car Game");
-    InitAudioDevice();
+	//Inicializacion de ventana (Y OpenGL).
+	int WindowWidth = 960;
+	int WindowHeigh = 540;
+	InitWindow(WindowWidth, WindowHeigh, "Car Game");
+	InitAudioDevice();
 
-    /////////////////////START STATE///////////////////////
-    // 
-    //CARGA DE FLIPBOOKS:
-    //Creacion de FlipBook para el background y Rectangles para posicionar Flipbook.
-    C_FlipBook FB_StartBackGround("resources/SpriteSheets/BackGround/StartBackgroundSheet.png", DARKBROWN, 42);
-    //COMENTARIO AL PROFESOR: Lei de un archivo txt que puedo meter todas las coordenadas y luego leerlas en C++ para no hacer todo esto pero al ser pocos Sprites y tener el tiempo ajustado no lo he hecho.
-    *FB_StartBackGround.SheetRec[0] = {600, 0, 600, 338};
-    *FB_StartBackGround.SheetRec[1] = {1200, 0, 600, 338 };
-    *FB_StartBackGround.SheetRec[2] = {1800, 0, 600, 338 };
-    *FB_StartBackGround.SheetRec[3] = {0, 338, 600, 338 };
-    *FB_StartBackGround.SheetRec[4] = {600, 338, 600, 338 };
-    *FB_StartBackGround.SheetRec[5] = {1200, 338, 600, 338 };
-    *FB_StartBackGround.SheetRec[6] = {1800, 338, 600, 338 };
-    *FB_StartBackGround.SheetRec[7] = {0, 676, 600, 338 };
-    *FB_StartBackGround.SheetRec[8] = {600, 676, 600, 338 };
-    *FB_StartBackGround.SheetRec[9] = {1200, 676, 600, 338 };
-    *FB_StartBackGround.SheetRec[10] = {1800, 676, 600, 338 };
-    *FB_StartBackGround.SheetRec[11] = {0, 1014, 600, 338 };
-    *FB_StartBackGround.SheetRec[12] = {600, 1014, 600, 338 };
-    *FB_StartBackGround.SheetRec[13] = {1200, 1014, 600, 338 };
-    *FB_StartBackGround.SheetRec[14] = {1800, 1014, 600, 338 };
-    *FB_StartBackGround.SheetRec[15] = {0, 1352, 600, 338 };
-    *FB_StartBackGround.SheetRec[16] = {600, 1352, 600, 338 };
-    *FB_StartBackGround.SheetRec[17] = {1200, 1352, 600, 338 };
-    *FB_StartBackGround.SheetRec[18] = {1800, 1352, 600, 338 };
-    *FB_StartBackGround.SheetRec[19] = {0, 1690, 600, 338 };
-    *FB_StartBackGround.SheetRec[20] = {600, 1690, 600, 338 };
-    *FB_StartBackGround.SheetRec[21] = {1200, 1690, 600, 338 };
-    *FB_StartBackGround.SheetRec[22] = {1800, 1690, 600, 338 };
-    *FB_StartBackGround.SheetRec[23] = {0, 2028, 600, 338 };
-    *FB_StartBackGround.SheetRec[24] = {600, 2028, 600, 338 };
-    *FB_StartBackGround.SheetRec[25] = {1200, 2028, 600, 338 };
-    *FB_StartBackGround.SheetRec[26] = {1200, 2028, 600, 338 };
-    *FB_StartBackGround.SheetRec[27] = {1800, 2028, 600, 338 };
-    *FB_StartBackGround.SheetRec[28] = {0, 2366, 600, 338 };
-    *FB_StartBackGround.SheetRec[29] = {600, 2366, 600, 338 };
-    *FB_StartBackGround.SheetRec[30] = {1200, 2366, 600, 338 };
-    *FB_StartBackGround.SheetRec[31] = {1800, 2366, 600, 338 };
-    *FB_StartBackGround.SheetRec[32] = {2400, 0, 600, 338 };
-    *FB_StartBackGround.SheetRec[33] = {2400, 338, 600, 338 };
-    *FB_StartBackGround.SheetRec[34] = {2400, 338, 600, 338 };
-    *FB_StartBackGround.SheetRec[35] = {2400, 676, 600, 338 };
-    *FB_StartBackGround.SheetRec[36] = {2400, 1014, 600, 338 };
-    *FB_StartBackGround.SheetRec[37] = {2400, 1352, 600, 338 };
-    *FB_StartBackGround.SheetRec[38] = {2400, 1690, 600, 338 };
-    *FB_StartBackGround.SheetRec[39] = {2400, 2028, 600, 338 };
-    *FB_StartBackGround.SheetRec[40] = {2400, 2366, 600, 338 };
-    *FB_StartBackGround.SheetRec[41] = {0, 2704, 600, 338 };
-    FB_StartBackGround.Transform = { 0.0, 0.0, (float)WindowWidth, (float)WindowHeigh};
+	/////////////////////START STATE///////////////////////
+	// 
+	//CARGA DE FLIPBOOKS:
+	//Creacion de FlipBook para el background y Rectangles para posicionar Flipbook.
+	C_FlipBook FB_StartBackGround("resources/SpriteSheets/BackGround/StartBackgroundSheet.png", DARKBROWN, 42);
+	//COMENTARIO AL PROFESOR: Lei de un archivo txt que puedo meter todas las coordenadas y luego leerlas en C++ para no hacer todo esto pero al ser pocos Sprites y tener el tiempo ajustado no lo he hecho.
+	*FB_StartBackGround.SheetRec[0] = { 600, 0, 600, 338 };
+	*FB_StartBackGround.SheetRec[1] = { 1200, 0, 600, 338 };
+	*FB_StartBackGround.SheetRec[2] = { 1800, 0, 600, 338 };
+	*FB_StartBackGround.SheetRec[3] = { 0, 338, 600, 338 };
+	*FB_StartBackGround.SheetRec[4] = { 600, 338, 600, 338 };
+	*FB_StartBackGround.SheetRec[5] = { 1200, 338, 600, 338 };
+	*FB_StartBackGround.SheetRec[6] = { 1800, 338, 600, 338 };
+	*FB_StartBackGround.SheetRec[7] = { 0, 676, 600, 338 };
+	*FB_StartBackGround.SheetRec[8] = { 600, 676, 600, 338 };
+	*FB_StartBackGround.SheetRec[9] = { 1200, 676, 600, 338 };
+	*FB_StartBackGround.SheetRec[10] = { 1800, 676, 600, 338 };
+	*FB_StartBackGround.SheetRec[11] = { 0, 1014, 600, 338 };
+	*FB_StartBackGround.SheetRec[12] = { 600, 1014, 600, 338 };
+	*FB_StartBackGround.SheetRec[13] = { 1200, 1014, 600, 338 };
+	*FB_StartBackGround.SheetRec[14] = { 1800, 1014, 600, 338 };
+	*FB_StartBackGround.SheetRec[15] = { 0, 1352, 600, 338 };
+	*FB_StartBackGround.SheetRec[16] = { 600, 1352, 600, 338 };
+	*FB_StartBackGround.SheetRec[17] = { 1200, 1352, 600, 338 };
+	*FB_StartBackGround.SheetRec[18] = { 1800, 1352, 600, 338 };
+	*FB_StartBackGround.SheetRec[19] = { 0, 1690, 600, 338 };
+	*FB_StartBackGround.SheetRec[20] = { 600, 1690, 600, 338 };
+	*FB_StartBackGround.SheetRec[21] = { 1200, 1690, 600, 338 };
+	*FB_StartBackGround.SheetRec[22] = { 1800, 1690, 600, 338 };
+	*FB_StartBackGround.SheetRec[23] = { 0, 2028, 600, 338 };
+	*FB_StartBackGround.SheetRec[24] = { 600, 2028, 600, 338 };
+	*FB_StartBackGround.SheetRec[25] = { 1200, 2028, 600, 338 };
+	*FB_StartBackGround.SheetRec[26] = { 1200, 2028, 600, 338 };
+	*FB_StartBackGround.SheetRec[27] = { 1800, 2028, 600, 338 };
+	*FB_StartBackGround.SheetRec[28] = { 0, 2366, 600, 338 };
+	*FB_StartBackGround.SheetRec[29] = { 600, 2366, 600, 338 };
+	*FB_StartBackGround.SheetRec[30] = { 1200, 2366, 600, 338 };
+	*FB_StartBackGround.SheetRec[31] = { 1800, 2366, 600, 338 };
+	*FB_StartBackGround.SheetRec[32] = { 2400, 0, 600, 338 };
+	*FB_StartBackGround.SheetRec[33] = { 2400, 338, 600, 338 };
+	*FB_StartBackGround.SheetRec[34] = { 2400, 338, 600, 338 };
+	*FB_StartBackGround.SheetRec[35] = { 2400, 676, 600, 338 };
+	*FB_StartBackGround.SheetRec[36] = { 2400, 1014, 600, 338 };
+	*FB_StartBackGround.SheetRec[37] = { 2400, 1352, 600, 338 };
+	*FB_StartBackGround.SheetRec[38] = { 2400, 1690, 600, 338 };
+	*FB_StartBackGround.SheetRec[39] = { 2400, 2028, 600, 338 };
+	*FB_StartBackGround.SheetRec[40] = { 2400, 2366, 600, 338 };
+	*FB_StartBackGround.SheetRec[41] = { 0, 2704, 600, 338 };
+	FB_StartBackGround.Transform = { 0.0, 0.0, (float)WindowWidth, (float)WindowHeigh };
 
-    C_FlipBook FB_StartControls("resources/SpriteSheets/BackGround/controls.png", WHITE, 1);
-    *FB_StartControls.SheetRec[0] = {0, 0, (float)FB_StartControls.SpriteSheet.width,(float)FB_StartControls.SpriteSheet.height};
-    FB_StartControls.Transform = {-100, 250, (float)WindowWidth - 450, (float)WindowHeigh - 220 };
+	C_FlipBook FB_StartControls("resources/SpriteSheets/BackGround/controls.png", WHITE, 1);
+	*FB_StartControls.SheetRec[0] = { 0, 0, (float)FB_StartControls.SpriteSheet.width,(float)FB_StartControls.SpriteSheet.height };
+	FB_StartControls.Transform = { -100, 250, (float)WindowWidth - 450, (float)WindowHeigh - 220 };
 
-    C_FlipBook FB_StartPlayStart("resources/SpriteSheets/BackGround/playStartSheet.png", WHITE, 6);
-    for (int i = 0; i < FB_StartPlayStart.TotalSprites; i++)
-    {
-        *FB_StartPlayStart.SheetRec[i] = { (600 * (float)i), 0, 600, 400 };
-    };
-    FB_StartPlayStart.Transform = { 190, 0, 600,300 };
+	C_FlipBook FB_StartPlayStart("resources/SpriteSheets/BackGround/playStartSheet.png", WHITE, 6);
+	for (int i = 0; i < FB_StartPlayStart.TotalSprites; i++)
+	{
+		*FB_StartPlayStart.SheetRec[i] = { (600 * (float)i), 0, 600, 400 };
+	};
+	FB_StartPlayStart.Transform = { 190, 0, 600,300 };
 
-    //CARGA DE SONIDO:
-    Sound StartThemeSound = LoadSound("resources/Sounds/Start/themeSong.wav");
+	//CARGA DE SONIDO:
+	Sound StartThemeSound = LoadSound("resources/Sounds/Start/themeSong.wav");
 
-    /////////////////////INGAME STATE///////////////////////
-    //CARGA DE FLIPBOOKS:
-    C_FlipBook FB_InGameBackground("resources/SpriteSheets/Race/raceSheet.png", WHITE, 4);
-    for (int i = 0; i < FB_InGameBackground.TotalSprites; i++)
-    {
-        *FB_InGameBackground.SheetRec[i] = { (320 * (float)i), 0, 320, 200};
-    };
-    FB_InGameBackground.Transform = { 0, 0, (float)WindowWidth, (float)WindowHeigh };
+	/////////////////////INGAME STATE///////////////////////
+	//CARGA DE FLIPBOOKS:
+	C_FlipBook FB_InGameBackground("resources/SpriteSheets/Race/raceSheet.png", WHITE, 4);
+	for (int i = 0; i < FB_InGameBackground.TotalSprites; i++)
+	{
+		*FB_InGameBackground.SheetRec[i] = { (320 * (float)i), 0, 320, 200 };
+	};
+	FB_InGameBackground.Transform = { 0, 0, (float)WindowWidth, (float)WindowHeigh };
 
-    //Trees:
-    const short TotalLTrees = 5;
-    short LeftTreesStartPos = 70;
-    C_FlipBook* LTrees = new C_FlipBook[TotalLTrees]{
-    C_FlipBook("resources/SpriteSheets/Race/tree.png", WHITE, 1),
-    C_FlipBook("resources/SpriteSheets/Race/tree.png", WHITE, 1),
-    C_FlipBook("resources/SpriteSheets/Race/tree.png", WHITE, 1),
-    C_FlipBook("resources/SpriteSheets/Race/tree.png", WHITE, 1),
-    C_FlipBook("resources/SpriteSheets/Race/tree.png", WHITE, 1),
-    };
-    for (int i = 0; i < TotalLTrees; i++)
-    {
-        LTrees[i].SheetRec[0]->x = 0;
-        LTrees[i].SheetRec[0]->y = 0;
-        LTrees[i].SheetRec[0]->width = 32;
-        LTrees[i].SheetRec[0]->height = 23;
-        LTrees[i].Transform.x = LeftTreesStartPos + GetRandomValue(70 * -1, 70);
-        LTrees[i].Transform.y = GetRandomValue(0 * -1, 500);
-        LTrees[i].Transform.width = 42;
-        LTrees[i].Transform.height = 33;
-    };
+	//Trees:
+	//TO-DO: Si usas la desviacion de los árboles con la Y hacia arriba, spawnearan con una Y diferente y no se notará porque estará por fuera.
+	//Cambiar además la barra de gasolina por una de progreso que vaya acordé a la puntuación final. Por lo cual el juego tendrá WIN.
+	const short TotalLTrees = 5;
+	short LeftTreesStartPos = 70;
+	C_FlipBook* LTrees = new C_FlipBook[TotalLTrees]{
+	C_FlipBook("resources/SpriteSheets/Race/tree.png", WHITE, 1),
+	C_FlipBook("resources/SpriteSheets/Race/tree.png", WHITE, 1),
+	C_FlipBook("resources/SpriteSheets/Race/tree.png", WHITE, 1),
+	C_FlipBook("resources/SpriteSheets/Race/tree.png", WHITE, 1),
+	C_FlipBook("resources/SpriteSheets/Race/tree.png", WHITE, 1),
+	};
+	for (int i = 0; i < TotalLTrees; i++)
+	{
+		LTrees[i].SheetRec[0]->x = 0;
+		LTrees[i].SheetRec[0]->y = 0;
+		LTrees[i].SheetRec[0]->width = 32;
+		LTrees[i].SheetRec[0]->height = 23;
+		LTrees[i].Transform.x = LeftTreesStartPos + GetRandomValue(70 * -1, 70);
+		LTrees[i].Transform.y = GetRandomValue(0 * -1, 500);
+		LTrees[i].Transform.width = 42;
+		LTrees[i].Transform.height = 33;
+	};
 
-    const short TotalRTrees = 5;
-    short RightTreesStartPos = 660;
-    C_FlipBook* RTrees = new C_FlipBook[TotalRTrees]{
-    C_FlipBook("resources/SpriteSheets/Race/tree.png", WHITE, 1),
-    C_FlipBook("resources/SpriteSheets/Race/tree.png", WHITE, 1),
-    C_FlipBook("resources/SpriteSheets/Race/tree.png", WHITE, 1),
-    C_FlipBook("resources/SpriteSheets/Race/tree.png", WHITE, 1),
-    C_FlipBook("resources/SpriteSheets/Race/tree.png", WHITE, 1),
-    };
-    for (int i = 0; i < TotalRTrees; i++)
-    {
-        RTrees[i].SheetRec[0]->x = 0;
-        RTrees[i].SheetRec[0]->y = 0;
-        RTrees[i].SheetRec[0]->width = 32;
-        RTrees[i].SheetRec[0]->height = 23;
-        RTrees[i].Transform.x = RightTreesStartPos + GetRandomValue(70 * -1, 70);
-        RTrees[i].Transform.y = GetRandomValue(0 * -1, 500);
-        RTrees[i].Transform.width = 42;
-        RTrees[i].Transform.height = 33;
-    };
+	const short TotalRTrees = 5;
+	short RightTreesStartPos = 660;
+	C_FlipBook* RTrees = new C_FlipBook[TotalRTrees]{
+	C_FlipBook("resources/SpriteSheets/Race/tree.png", WHITE, 1),
+	C_FlipBook("resources/SpriteSheets/Race/tree.png", WHITE, 1),
+	C_FlipBook("resources/SpriteSheets/Race/tree.png", WHITE, 1),
+	C_FlipBook("resources/SpriteSheets/Race/tree.png", WHITE, 1),
+	C_FlipBook("resources/SpriteSheets/Race/tree.png", WHITE, 1),
+	};
+	for (int i = 0; i < TotalRTrees; i++)
+	{
+		RTrees[i].SheetRec[0]->x = 0;
+		RTrees[i].SheetRec[0]->y = 0;
+		RTrees[i].SheetRec[0]->width = 32;
+		RTrees[i].SheetRec[0]->height = 23;
+		RTrees[i].Transform.x = RightTreesStartPos + GetRandomValue(70 * -1, 70);
+		RTrees[i].Transform.y = GetRandomValue(0 * -1, 500);
+		RTrees[i].Transform.width = 42;
+		RTrees[i].Transform.height = 33;
+	};
 
-    //Player Vehicle:
-    C_Vehicle RedCar("resources/SpriteSheets/Race/redCar.png",1);
-    RedCar.Vehicle.SheetRec[0]->x = 0;
-    RedCar.Vehicle.SheetRec[0]->y = 0;
-    RedCar.Vehicle.SheetRec[0]->width = 28;
-    RedCar.Vehicle.SheetRec[0]->height = 54;
+	//Player Vehicle:
+	C_Vehicle RedCar("resources/SpriteSheets/Race/redCar.png", 1);
+	RedCar.Vehicle.SheetRec[0]->x = 0;
+	RedCar.Vehicle.SheetRec[0]->y = 0;
+	RedCar.Vehicle.SheetRec[0]->width = 28;
+	RedCar.Vehicle.SheetRec[0]->height = 54;
 
-    RedCar.Vehicle.Transform.x = WindowWidth/2 - 113;
-    RedCar.Vehicle.Transform.y = 390;
-    RedCar.Vehicle.Transform.width = 42;
-    RedCar.Vehicle.Transform.height = 81;
+	RedCar.Vehicle.Transform.x = WindowWidth / 2 - 113;
+	RedCar.Vehicle.Transform.y = 390;
+	RedCar.Vehicle.Transform.width = 42;
+	RedCar.Vehicle.Transform.height = 81;
 
-    //Static Enemy Vehicle:
-    //TO-DO: Averiguar como spawnear de manera dinamica coches enemigos y que sean eliminados al llegar al final. Luego de ello tener una probabilidad de spawn y desviacion en el eje X de la carretera. Todo esto trabajarlo en sus respectivas clases como la del vehiculo enemigo.
-    C_Enemy_Vehicle YellowTruck("resources/SpriteSheets/Race/yellowTruck.png",1);
+	//Static Enemy Vehicle:
+	//TO-DO: Crear un sistema de probabilidades de Spawn y crear colisiones.
+	C_Enemy_Vehicle YellowCars("resources/SpriteSheets/Race/yellowCar.png", 1, 10);
+	YellowCars.Spawn(200, 30);
+	YellowCars.Spawn(400, 300);
 
-    //Game Bucle:
-    E_GamePlayState GamePlayState = InGame;
-    HideCursor();
-    SetTargetFPS(165);
+	//Game Bucle:
+	E_GamePlayState GamePlayState = InGame;
+	HideCursor();
+	SetTargetFPS(165);
 
-    while (globalRunning)
-    {    
-        
-        BeginDrawing();
+	while (globalRunning)
+	{
 
-        switch (GamePlayState)
-        {
-        case Start:
-            if (!IsSoundPlaying(StartThemeSound)) {
-                PlaySound(StartThemeSound);
-            };
-                FB_StartBackGround.Play(0.04, 0.0, false);
-                FB_StartControls.Play(0, 0.0, false);
-                FB_StartPlayStart.Play(0.1, 0, false);
-                if (GetKeyPressed() || IsMouseButtonDown(MOUSE_BUTTON_LEFT) || IsMouseButtonDown(MOUSE_BUTTON_RIGHT)) {
-                    GamePlayState = InGame;
-                    StopSound(StartThemeSound);
-                };
-            break;
-        case InGame:
-           
-                FB_InGameBackground.Play(0.1, 0, false);
+		BeginDrawing();
 
-            for (int i = 0; i < TotalLTrees; i++)
-            {
-                LTrees[i].Play(0, 0, false);
-                LTrees[i].MoveTo(0, 40, 0.12);
-                LTrees[i].ClampPos(0, 500, 70, 10, 70, 0);
-            }
+		switch (GamePlayState)
+		{
+		case Start:
+			if (!IsSoundPlaying(StartThemeSound)) {
+				PlaySound(StartThemeSound);
+			};
+			FB_StartBackGround.Play(0.04, 0.0, false);
+			FB_StartControls.Play(0, 0.0, false);
+			FB_StartPlayStart.Play(0.1, 0, false);
+			if (GetKeyPressed() || IsMouseButtonDown(MOUSE_BUTTON_LEFT) || IsMouseButtonDown(MOUSE_BUTTON_RIGHT)) {
+				GamePlayState = InGame;
+				StopSound(StartThemeSound);
+			};
+			break;
+		case InGame:
 
-            for (int i = 0; i < TotalRTrees; i++)
-            {
-                RTrees[i].Play(0, 0, false);
-                RTrees[i].MoveTo(0, 40, 0.12);
-                RTrees[i].ClampPos(0, 500, RightTreesStartPos, 10, 70, 0);
-            }
-            RedCar.Vehicle.Play(0, 0, true);
+			FB_InGameBackground.Play(0.1, 0, false);
 
-            //Car Movement:
-            
-            if (IsKeyDown(KEY_W)) {
-                RedCar.Vehicle.MoveTo(0, -3, 0.01);
-            }
-            else if (IsKeyDown(KEY_S)) {
-                RedCar.Vehicle.MoveTo(0, 3, 0.01);
-            }
-            else if (IsKeyDown(KEY_A)) {
-                RedCar.Vehicle.MoveTo(-3, 0, 0.01);
-            }
-            else if (IsKeyDown(KEY_D)) {
-                RedCar.Vehicle.MoveTo(3, 0, 0.01);
-            }
-            RedCar.RestrainToRoads();
-        case Dead:
-            break;
-        };
-     
-        /*DrawFPS(850, 20);*/
-        EndDrawing();
-        ClearBackground(BLACK);
+			for (int i = 0; i < TotalLTrees; i++)
+			{
+				LTrees[i].Play(0, 0, false);
+				LTrees[i].MoveTo(0, 40, 0.12);
+				LTrees[i].ClampPos(0, 500, 70, 10, 70, 0);
+			}
 
-        //Esto verifica si se debe cerrar la ventana al pulsar ESC o la X.
-        if (WindowShouldClose()) {
-            globalRunning = false;
-            CloseWindow();
-        };
-    };
+			for (int i = 0; i < TotalRTrees; i++)
+			{
+				RTrees[i].Play(0, 0, false);
+				RTrees[i].MoveTo(0, 40, 0.12);
+				RTrees[i].ClampPos(0, 500, RightTreesStartPos, 10, 70, 0);
+			}
+			RedCar.Vehicle.Play(0, 0, false);
 
-    return 0;
+			//Car Movement:
+
+			if (IsKeyDown(KEY_W)) {
+				RedCar.Vehicle.MoveTo(0, -3, 0.01);
+			}
+			else if (IsKeyDown(KEY_S)) {
+				RedCar.Vehicle.MoveTo(0, 3, 0.01);
+			}
+			else if (IsKeyDown(KEY_A)) {
+				RedCar.Vehicle.MoveTo(-3, 0, 0.01);
+			}
+			else if (IsKeyDown(KEY_D)) {
+				RedCar.Vehicle.MoveTo(3, 0, 0.01);
+			}
+			RedCar.RestrainToRoads();
+
+			for (int i = YellowCars.TotalEnemys-1; i >= 0; i--) {
+
+				YellowCars.Vehicles[i]->Play(0, 0, false);
+				YellowCars.Vehicles[i]->MoveTo(0, 2, 0.01);
+				YellowCars.DeleteAtPos(0, 560, i);
+			}
+		case Dead:
+			break;
+		};
+
+		/*DrawFPS(850, 20);*/
+		EndDrawing();
+		ClearBackground(BLACK);
+
+		//Esto verifica si se debe cerrar la ventana al pulsar ESC o la X.
+		if (WindowShouldClose()) {
+			globalRunning = false;
+			CloseWindow();
+		};
+	};
+
+	return 0;
 };
--- game/src/main.h
+++ game/src/main.h
@@ -8,7 +8,7 @@ private:
 	int CurrentFrame;
 	Vector2 Position;
 	Color SpritesColor;
-	
+
 public:
 	Texture2D SpriteSheet;
 	const short TotalSprites;
@@ -32,37 +32,37 @@ public:
 		{
 			SheetRec[i] = new Rectangle; //Asignacion de instancias Rectangle a los punteros.
 		};
-			
+
 	};
 	~C_FlipBook() {
 		UnloadTexture(SpriteSheet);
 
-		
+
 		for (int i = 0; i != TotalSprites; i++) //Eliminacion de memoria tanto los punteros de las instancias y del puntero principal.
 		{
 			delete SheetRec[i];
 		};
-		delete SheetRec; 
+		delete SheetRec;
 	};
 	void Play(float PlayRate, float Rotation, bool Debug) {
-			AnimSecTimer += GetFrameTime();
-			DrawTexturePro(SpriteSheet, *SheetRec[CurrentFrame], Transform, Position, Rotation, SpritesColor);
-			if (AnimSecTimer > PlayRate) {
-				CurrentFrame++;
+		AnimSecTimer += GetFrameTime();
+		DrawTexturePro(SpriteSheet, *SheetRec[CurrentFrame], Transform, Position, Rotation, SpritesColor);
+		if (AnimSecTimer > PlayRate) {
+			CurrentFrame++;
 
-				AnimSecTimer = 0.0f;
+			AnimSecTimer = 0.0f;
 		};
-			if (CurrentFrame == TotalSprites) {
-				CurrentFrame = 0;
-			};
-			// v Display de FPS del FlipBook para debug. v
-			if (Debug) {
-				char DebugText[100];
-				sprintf(DebugText, "%d", CurrentFrame);
-				DrawText(DebugText, 0, 0, 20, RED);
-			};
-			// ^ Display de FPS del FlipBook para debug.^
-	};	
+		if (CurrentFrame == TotalSprites) {
+			CurrentFrame = 0;
+		};
+		// v Display de FPS del FlipBook para debug. v
+		if (Debug) {
+			char DebugText[100];
+			sprintf(DebugText, "%d", CurrentFrame);
+			DrawText(DebugText, 0, 0, 20, RED);
+		};
+		// ^ Display de FPS del FlipBook para debug.^
+	};
 
 	void MoveTo(float AddX, float AddY, float MoveRate) {
 		MoveSecTimer += GetFrameTime();
@@ -71,7 +71,7 @@ public:
 			Transform.y += AddY;
 
 			MoveSecTimer = 0;
-		}		
+		}
 	}
 
 	void ClampPos(float MaxX, float MaxY, float MoveToX, float MoveToY, int XDesviation, int YDesviation) {
@@ -86,7 +86,7 @@ public:
 			Transform.y += GetRandomValue(YDesviation * -1, YDesviation);
 			Transform.x = MoveToX;
 			Transform.x += GetRandomValue(XDesviation * -1, XDesviation);
-		};	
+		};
 	};
 
 };
@@ -100,22 +100,55 @@ public:
 	C_Vehicle(char* VehicleFlipBook_, int FlipBookFrames_) : VehicleFlipBook(VehicleFlipBook_) { //Uso de inicializacion de variables miembro antes del cuerpo del constructor para poder usarlas en la clase C_FlipBook.
 		FlipBookFrames = FlipBookFrames_;
 	};
+	~C_Vehicle() {};
 
 	void RestrainToRoads() {
-	if (Vehicle.Transform.x > 540) { Vehicle.Transform.x = 540; }
-	if (Vehicle.Transform.x < 192) { Vehicle.Transform.x = 192; }
-	if (Vehicle.Transform.y < 10) { Vehicle.Transform.y = 10; }
-	if (Vehicle.Transform.y > 440) { Vehicle.Transform.y = 440; }
+		if (Vehicle.Transform.x > 540) { Vehicle.Transform.x = 540; }
+		if (Vehicle.Transform.x < 192) { Vehicle.Transform.x = 192; }
+		if (Vehicle.Transform.y < 10) { Vehicle.Transform.y = 10; }
+		if (Vehicle.Transform.y > 440) { Vehicle.Transform.y = 440; }
 	}
 };
 
-class C_Enemy_Vehicle : public C_Vehicle {
+class C_Enemy_Vehicle {
 private:
 	char* VehicleFlipBook;
 	int FlipBookFrames;
 public:
-	C_Enemy_Vehicle(char* VehicleFlipBook_, int FlipBookFrames_) : C_Vehicle(VehicleFlipBook_, FlipBookFrames_) {
-	
+	short TotalEnemys = 0;
+	short MaxEnemys;
+	C_FlipBook** Vehicles;
+	C_Enemy_Vehicle(char* VehicleFlipBook_, int FlipBookFrames_, short MaxEnemys_) : VehicleFlipBook(VehicleFlipBook_), FlipBookFrames(FlipBookFrames_), MaxEnemys(MaxEnemys_) {
+		Vehicles = new C_FlipBook * [MaxEnemys];
+	}
+	void Spawn(short PosX, short PosY) {
+		Vehicles[TotalEnemys] = new C_FlipBook("resources/SpriteSheets/Race/yellowCar.png", WHITE, 1);
+		Vehicles[TotalEnemys]->SheetRec[0]->x = 0;
+		Vehicles[TotalEnemys]->SheetRec[0]->y = 0;
+		Vehicles[TotalEnemys]->SheetRec[0]->width = 28;
+		Vehicles[TotalEnemys]->SheetRec[0]->height = 69;
+
+		Vehicles[TotalEnemys]->Transform.x = PosX;
+		Vehicles[TotalEnemys]->Transform.y = PosY;
+		Vehicles[TotalEnemys]->Transform.width = 42;
+		Vehicles[TotalEnemys]->Transform.height = 81;
+
+		TotalEnemys++;
+	}
+	void DeleteAtPos(short MaxX, short MaxY, short VehicleIndex) {
+		if (MaxX != 0 && Vehicles[VehicleIndex]->Transform.x > MaxX) {
+			// /!\ Void
+		}
+		if (MaxY != 0 && Vehicles[VehicleIndex]->Transform.y > MaxY) {
+			delete Vehicles[VehicleIndex];
+			for (int i = VehicleIndex; i < TotalEnemys; i++) //Realiaza una redimension del array ya que al eliminarse de la memoria el objeto, este deja un hueco en el array. Por lo cual con este algoritmo remplazamos el valor de adelante por el vacio asi sucesivamente con todos hasta que el hueco quede al final. Ya con el control total del array podremos cerrar el hueco final.
+			{
+				Vehicles[i] = Vehicles[i + 1];
+			}
+			TotalEnemys--;
+
+			Spawn(500, -100);
+		}
 	}
 };
 
